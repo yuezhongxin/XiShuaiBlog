552d24881fcb706d300f821ee308b85b3b3a81ae
yuezhongxin <yuezhongxin@gmail.com>
2014-03-27 18:56:20 +0800

    添加EasyUI集合KindEditor

---
+++ Project/Demo/easyUI/EasyUIDemo/[2]EasyUI集合KindEditor/EasyUIDemo/WebForm1.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EasyUIDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <link href="EasyUI/themes/default/easyui.css" rel="stylesheet" />
+    <link href="EasyUI/themes/icon.css" rel="stylesheet" />
+    <link href="css/admin.css" rel="stylesheet" />
+    <script src="EasyUI/jquery.min.js"></script>
+    <script src="EasyUI/jquery.easyui.min.js"></script>
+</head>
+<body class="easyui-layout">
+    <div data-options="region:'north',border:false" style="height: 74px; background: #2596ea; padding: 10px">
+        <img src="images/logo.png" />EasyUIDemo
+    </div>
+    <div data-options="region:'west',split:true,title:'导航菜单'" style="width: 170px;">
+        <div class="easyui-accordion" data-options="fit:true,border:false">
+            <div title="系统设置1" data-options="selected:true">
+                <div style="margin: 5px">
+                    <ul class="tree easyui-tree" data-options="animate:true,lines:true">
+                        <li data-options="iconCls:'icon-group'">
+                            <span>基本设置</span>
+                            <ul>
+                                <li data-options="iconCls:'icon-group_add'">
+                                    <span>test1</span>
+                                </li>
+                                <li data-options="iconCls:'icon-group_delete'">
+                                    <span>test2</span>
+                                </li>
+                                <li data-options="iconCls:'icon-group_edit'">
+                                    <span>test3</span>
+                                </li>
+                            </ul>
+                        </li>
+                        <li data-options="state:'closed',iconCls:'icon-joystick'">
+                            <span>系统设置</span>
+                            <ul>
+                                <li data-options="iconCls:'icon-joystick_add'">
+                                    <span>test4</span>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div title="系统设置2" style="padding: 10px;">
+                content2
+            </div>
+            <div title="系统设置3" style="padding: 10px">
+                content3
+            </div>
+        </div>
+    </div>
+    <div data-options="region:'south',border:false" style="height: 23px;">
+        <div class="footer">EasyUIDemo</div>
+    </div>
+    <div data-options="region:'center'">
+        <div id="tabs" class="easyui-tabs" data-options="tools:'#tab-tools'">
+            <div title="主页" data-options="iconCls:'icon-house'" style="padding: 10px; height: 100%;">主页</div>
+        </div>
+        <div id="tab-tools">
+            <a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-add'" onclick="addPanel()"></a>
+            <a href="javascript:void(0)" class="easyui-linkbutton" data-options="plain:true,iconCls:'icon-remove'" onclick="removePanel()"></a>
+        </div>
+    </div>
+    <script type="text/javascript">
+        var index = 0;
+        function addPanel() {
+            index++;
+            $('#tabs').tabs('add', {
+                title: 'Tab' + index,
+                content: '<div style="padding:10px">Content' + index + '</div>',
+                closable: true
+            });
+        }
+        function removePanel() {
+            var tab = $('#tabs').tabs('getSelected');
+            if (tab) {
+                var index = $('#tabs').tabs('getTabIndex', tab);
+                $('#tabs').tabs('close', index);
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ Project/Demo/easyUI/EasyUIDemo/[2]EasyUI集合KindEditor/EasyUIDemo/js/common.js
@@ -0,0 +1,138 @@
+﻿var onlyOpenTitle = "主页";//不允许关闭的标签的标题
+$(function () {
+    tabClose();
+    tabCloseEven();
+})
+
+function tabClose() {
+    /*双击关闭TAB选项卡*/
+    $(".tabs-inner").dblclick(function () {
+        var subtitle = $(this).children(".tabs-closable").text();
+        $('#tabs').tabs('close', subtitle);
+    })
+    /*为选项卡绑定右键*/
+    $(".tabs-inner").bind('contextmenu', function (e) {
+        $('#mm').menu('show', {
+            left: e.pageX,
+            top: e.pageY
+        });
+        var subtitle = $(this).children(".tabs-closable").text();
+        $('#mm').data("currtab", subtitle);
+        $('#tabs').tabs('select', subtitle);
+        return false;
+    });
+}
+//绑定右键菜单事件
+function tabCloseEven() {
+    $('#mm').menu({
+        onClick: function (item) {
+            closeTab(item.id);
+        }
+    });
+    return false;
+}
+
+//创建/移除tab
+var index = 0;
+function addTab(subtitle, url, icon) {
+    index++;
+    if (!$('#tabs').tabs('exists', subtitle)) {
+        $('#tabs').tabs('add', {
+            title: 'Tab' + index,
+            content: createFrame('http://www.baidu.com/'),
+            closable: true,
+            icon: ''
+        });
+    } else {
+        $('#tabs').tabs('select', subtitle);
+        $('#mm-tabupdate').click();
+    }
+    tabClose();
+}
+function removeTab() {
+    var tab = $('#tabs').tabs('getSelected');
+    if (tab) {
+        var index = $('#tabs').tabs('getTabIndex', tab);
+        $('#tabs').tabs('close', index);
+    }
+}
+//创建Frame
+function createFrame(url) {
+    var s = '<iframe scrolling="auto" frameborder="0"  src="' + url + '" style="width:100%;height:100%;"></iframe>';
+    return s;
+}
+
+//关闭tab
+function closeTab(action) {
+    var alltabs = $('#tabs').tabs('tabs');
+    var currentTab = $('#tabs').tabs('getSelected');
+    var allTabtitle = [];
+    $.each(alltabs, function (i, n) {
+        allTabtitle.push($(n).panel('options').title);
+    })
+    switch (action) {
+        case "refresh":
+            var iframe = $(currentTab.panel('options').content);
+            var src = iframe.attr('src');
+            $('#tabs').tabs('update', {
+                tab: currentTab,
+                options: {
+                    content: createFrame(src)
+                }
+            })
+            break;
+        case "close":
+            var currtab_title = currentTab.panel('options').title;
+            $('#tabs').tabs('close', currtab_title);
+            break;
+        case "closeall":
+            $.each(allTabtitle, function (i, n) {
+                if (n != onlyOpenTitle) {
+                    $('#tabs').tabs('close', n);
+                }
+            });
+            break;
+        case "closeother":
+            var currtab_title = currentTab.panel('options').title;
+            $.each(allTabtitle, function (i, n) {
+                if (n != currtab_title && n != onlyOpenTitle) {
+                    $('#tabs').tabs('close', n);
+                }
+            });
+            break;
+        case "closeright":
+            var tabIndex = $('#tabs').tabs('getTabIndex', currentTab);
+
+            if (tabIndex == alltabs.length - 1) {
+                alert('亲，后边没有啦 ^@^!!');
+                return false;
+            }
+            $.each(allTabtitle, function (i, n) {
+                if (i > tabIndex) {
+                    if (n != onlyOpenTitle) {
+                        $('#tabs').tabs('close', n);
+                    }
+                }
+            });
+            break;
+        case "closeleft":
+            var tabIndex = $('#tabs').tabs('getTabIndex', currentTab);
+            if (tabIndex == 1) {
+                alert('亲，前边那个上头有人，咱惹不起哦。 ^@^!!');
+                return false;
+            }
+            $.each(allTabtitle, function (i, n) {
+                if (i < tabIndex) {
+                    if (n != onlyOpenTitle) {
+                        $('#tabs').tabs('close', n);
+                    }
+                }
+            });
+            break;
+        case "exit":
+            $('#closeMenu').menu('hide');
+            break;
+    }
+}
+
+
