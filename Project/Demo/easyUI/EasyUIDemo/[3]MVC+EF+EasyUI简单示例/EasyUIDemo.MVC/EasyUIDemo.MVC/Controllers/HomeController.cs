86e97fadde56dd8c276bf9938c87f2ba429a440d
yuezhongxin <yuezhongxin@gmail.com>
2014-03-30 23:34:43 +0800

    新增MVC+EF+EasyUI简单示例

---
+++ Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyUIDemo.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// iframe获取视图
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetView(string viewPara)
+        {
+            return View(viewPara);
+        }
+    }
+}
---
+++ Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/Controllers/LoginController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EasyUIDemo.Model;
+
+namespace EasyUIDemo.MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            //获取到Cookie中的值传递给前台显示
+            var UserName = Request.Cookies["UserName"] == null ? "" : Request.Cookies["UserName"].Value;
+            ViewBag.UserName = UserName;
+            return View();
+        }
+
+        /// <summary>
+        /// 处理登录的信息
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <param name="Code"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //public JsonResult CheckUserLogin(UserInfo userInfo)
+        //{
+        //    using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+        //    {
+        //        //linq查询
+        //        var users = from p in db.UserInfo
+        //                    where p.Name == userInfo.Name && p.Password == userInfo.Password
+        //                    select p;
+        //        if (users.Count() > 0)
+        //        {
+        //            userInfo = users.FirstOrDefault();
+        //            Response.Cookies["UserName"].Value = userInfo.Name;
+        //            Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(7);
+        //            return Json(new { result = "success", content = "" });
+        //        }
+        //        else
+        //        {
+        //            return Json(new { result = "error", content = "用户名密码错误，请您检查" });
+        //        }
+        //    }
+        //}
+
+        public string CheckUserLogin(string Name, string Password)
+        {
+            return "success";
+
+            //using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+            //{
+            //    //linq查询
+            //    var users = from p in db.UserInfo
+            //                where p.Name == userInfo.Name && p.Password == userInfo.Password
+            //                select p;
+            //    if (users.Count() > 0)
+            //    {
+            //        userInfo = users.FirstOrDefault();
+            //        Response.Cookies["UserName"].Value = userInfo.Name;
+            //        Response.Cookies["UserName"].Expires = DateTime.Now.AddDays(7);
+            //        return "success";
+            //        //return Json(new { result = "success", content = "" });
+            //    }
+            //    else
+            //    {
+            //        return "用户名密码错误，请您检查";
+            //        //return Json(new { result = "error", content = "用户名密码错误，请您检查" });
+            //    }
+            //}
+        }
+    }
+}
