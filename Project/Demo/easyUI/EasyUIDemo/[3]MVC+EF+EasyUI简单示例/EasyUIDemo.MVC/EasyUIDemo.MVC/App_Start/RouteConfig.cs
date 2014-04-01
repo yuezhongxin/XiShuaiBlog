cc879ee145c7c836b9565497dd33ad9d996ac3ff
yuezhongxin <yuezhongxin@gmail.com>
2014-04-01 18:29:17 +0800

    修改EasyUIDemo.MVC

--- Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/App_Start/RouteConfig.cs
+++ Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/App_Start/RouteConfig.cs
@@ -16,8 +16,8 @@ namespace EasyUIDemo.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
--- Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/Controllers/UserController.cs
+++ Project/Demo/easyUI/EasyUIDemo/[3]MVC+EF+EasyUI简单示例/EasyUIDemo.MVC/EasyUIDemo.MVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyUIDemo.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,100 @@ namespace EasyUIDemo.MVC.Controllers
         public ActionResult User_List()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 获取用户的所有信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAllUserInfo()
+        {
+            int pageIndex = Request["page"] == null ? 1 : int.Parse(Request["page"]);
+            int pageSize = Request["rows"] == null ? 10 : int.Parse(Request["rows"]);
+            int total = 0;
+            using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+            {
+                var temp = from u in db.UserInfo select u;
+                total = temp.Count();
+                var users = temp.OrderByDescending(s => s.ID)
+                     .Skip<UserInfo>((pageIndex - 1) * pageSize).Take<UserInfo>(pageSize);
+                var data = new
+                {
+                    total = total,
+                    rows = users.ToList<UserInfo>()
+                };
+                return Json(data);
+            }
+        }
+
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        /// <param name="userinfo"></param>
+        /// <returns></returns>
+        public ActionResult AddUser(UserInfo userinfo)
+        {
+            userinfo.CreateTime = DateTime.Now;
+            using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+            {
+                db.UserInfo.Add(userinfo);
+                db.SaveChanges();
+            }
+            return Content("OK");
+        }
+
+        /// <summary>
+        /// 绑定用户数据
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public ActionResult GetBindDetails(int ID)
+        {
+            using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+            {
+                var BindIDForUpdateTextBox = db.UserInfo.Where(u => u.ID == ID).FirstOrDefault();
+                return Json(BindIDForUpdateTextBox, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// 删除用户
+        /// </summary>
+        /// <param name="deleteUserInfoID"></param>
+        /// <returns></returns>
+        public ActionResult DeleteUser(string deleteUserInfoID, string Name)
+        {
+            var UIdsName = Name.Split(',');
+            List<string> deleteUName = new List<string>();
+            foreach (var item in UIdsName)
+            {
+                deleteUName.Add(item);
+            }
+            if (deleteUName.Contains(Session["UserName"].ToString()))
+            {
+                return Content("含有正在使用的用户，禁止删除");
+            }
+
+            if (string.IsNullOrEmpty(deleteUserInfoID))
+            {
+                return Content("请选择您要删除的数据");
+            }
+            var idsStr = deleteUserInfoID.Split(',');
+            List<int> deleteIDList = new List<int>();
+            foreach (var ID in idsStr)
+            {
+                deleteIDList.Add(Convert.ToInt32(ID));
+            }
+            using (EasyUIDemoDBEntities db = new EasyUIDemoDBEntities())
+            {
+                foreach (var ID in deleteIDList)
+                {
+                    var users = db.UserInfo.Where(p => p.ID == ID).FirstOrDefault();
+                    db.UserInfo.Remove(users);
+                }
+                db.SaveChanges();
+            }
+            return Content("OK");
         }
     }
 }
